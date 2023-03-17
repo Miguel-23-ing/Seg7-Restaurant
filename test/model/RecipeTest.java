fbf6f5385dccf7c256ced5efd153f1d57b55dab6
Miguel-23-ing <miguelangelmartines148@gmail.com>
2023-03-17 11:02:06 -0500

    Cargue Lab 7

--- test/model/RecipeTest.java
+++ test/model/RecipeTest.java
@@ -5,10 +5,15 @@ public class RecipeTest {
     private Recipe rec;
 
     public void setUp1(){
-        Ingredient n = new Ingredient("",0);
+        rec = new Recipe();
+    }
 
-    }
     public void setUp2(){
+        Ingredient ingredient1 = new Ingredient("Cebolla", 315);
+        Ingredient ingredient2 = new Ingredient("Ajo", 58);
+        Ingredient ingredient3 = new Ingredient("Arroz", 520);
     }
+
+    
 
 }
